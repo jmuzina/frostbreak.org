882de2aabb4deb1e4e319a23786386c578ee4808
Julie Muzina <julie.muzina@canonical.com>
2025-03-08 00:41:39 -0500

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import {browserslistToTargets} from "lightningcss";
+import browserslist from "browserslist";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  vite: {
+    css: {
+      transformer: "lightningcss",
+      lightningcss: {
+        targets: browserslistToTargets(browserslist('>= 0.25%')),
+        drafts: {
+          customMedia: true
+        }
+      }
+    },
+    build: {
+      cssMinify: 'lightningcss'
+    }
+  },
 });
