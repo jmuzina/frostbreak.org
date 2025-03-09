7371b4fbe0d87adcd9278865cd4b80d62ad91ca7
Julie Muzina <jmuzina@jmuzina.io>
2025-03-09 15:24:36 -0400

    adjust github url

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const DISCORD_INVITE_LINK = "https://discord.gg/gnbAMtPymy";
 export const BASE_URL = "https://frostbreak.org";
+export const GITHUB_REPO_URL = "https://github.com/jmuzina/frostbreak.org";
