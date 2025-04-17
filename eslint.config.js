ecee173a7cdf1fb561a04d1d34452464f5b56c0f
Julie Muzina <jmuzina@jmuzina.io>
2025-04-17 02:19:57 -0400

    Add NHX experience client (#31)

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,14 @@ import jseslint from "@eslint/js";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["node_modules/", "dist/", ".astro/", ".vscode/", ".idea/"]),
+  globalIgnores([
+    "node_modules/",
+    "dist/",
+    ".astro/",
+    ".vscode/",
+    ".idea/",
+    "public/",
+  ]),
   // ...tseslint.config(
   //   tseslint.configs.recommended,
   //   jseslint.configs.recommended
