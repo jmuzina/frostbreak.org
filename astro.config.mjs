6e49d76a641a3c66300ba2309f08245ccb6b902f
Julie Muzina <jmuzina@jmuzina.io>
2025-06-07 23:27:00 -0400

    aaa

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,6 @@ const { PR_PREVIEW_PREFIX } = process.env;
 
 const base = PR_PREVIEW_PREFIX?.length ? PR_PREVIEW_PREFIX : "";
 
-console.log({ base });
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
